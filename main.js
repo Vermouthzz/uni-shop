dfdd395be1dfe9f1a18e7752e598a56acc4f3d58
LiuHaowei <2239827165@qq.com>
2023-05-15 20:45:54 +0800

    'home'

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 import App from './App'
 
+
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+uni.$showMeg = function(title = '数据加载成功', duration = 1500) {
+	uni.showTopWindow({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
